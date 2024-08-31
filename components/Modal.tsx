37ee20f7cc113ecad164e110abae20d7bfbd683b
briandangunsw <long.dang@student.unsw.edu.au>
2024-08-31 16:50:01 +1000

    added AuthModal and useAuthModal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,5 +1,5 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { IoMdClose } from "ract-icons/io";
+import { IoMdClose } from "react-icons/io";
 
 interface ModalProps {
   isOpen: boolean;
@@ -54,9 +54,50 @@ const Modal: React.FC<ModalProps> = ({
               focus:outline-none
             "
           >
-            <Dialog.Title>
+            <Dialog.Title
+              className="
+                text-xl
+                text-center
+                font-bold
+                mb-4
+              "
+            >
               {title}
             </Dialog.Title>
+            <Dialog.Description
+              className="
+                mb-5
+                text-sm
+                leading-normal
+                text-center
+              "
+            >
+              {description}
+            </Dialog.Description>
+            <div>
+              {children}
+            </div>
+            <Dialog.Close asChild>
+              <button
+                className="
+                  text-neutral-400
+                  hover:text-white
+                  absolute
+                  top-[10px]
+                  right-[10px]
+                  inline-flex
+                  h-[25px]
+                  w-[25px]
+                  appearance-none
+                  items-center
+                  justify-center
+                  rounded-full
+                  focus:outline-none
+                "
+                >
+                <IoMdClose />
+              </button>
+            </Dialog.Close>
           </Dialog.Content>
         </Dialog.Portal>
     </Dialog.Root>
