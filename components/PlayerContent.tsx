28d1af72637d631d4b52d8411ae2ce5f88724ee1
briandangunsw <long.dang@student.unsw.edu.au>
2024-09-12 15:49:03 +1000

    updated PlayerContent.tsx and added Slider.tsx

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -5,6 +5,10 @@ import MediaItem from "./MediaItem";
 import LikeButton from "./LikeButton";
 import { BsPauseFill, BsPlayFill } from "react-icons/bs";
 import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
+import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
+import Slider from "./Slider";
+import usePlayer from "@/hooks/usePlayer";
+import { useState } from "react";
 
 interface PlayerContentProps {
   song: Song;
@@ -15,7 +19,42 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
   song,
   songUrl
 }) => {
-  const Icon = true ? BsPauseFill : BsPlayFill;
+  const player = usePlayer();
+  const [volume, setVolume] = useState(1);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const Icon = isPlaying ? BsPauseFill : BsPlayFill;
+  const VolumeIcon = volume === 0 ? HiSpeakerXMark : HiSpeakerWave;
+
+  const onPlayNext = () => {
+    if (player.ids.length === 0) {
+      return;
+    }
+
+    const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+    const nextSong = player.ids[currentIndex + 1];
+
+    if (!nextSong) {
+      return player.setId(player.ids[0]);
+    }
+
+    player.setId(nextSong);
+  }
+
+  const onPlayPrevious = () => {
+    if (player.ids.length === 0) {
+      return;
+    }
+
+    const currentIndex = player.ids.findIndex((id) => id === player.activeId);
+    const previousSong = player.ids[currentIndex - 1];
+
+    if (!previousSong) {
+      return player.setId(player.ids[player.ids.length - 1]);
+    }
+
+    player.setId(previousSong);
+  }
 
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 h-full">
@@ -71,7 +110,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
         "
       >
         <AiFillStepBackward
-          onClick={() => {}}
+          onClick={onPlayPrevious}
           size={30}
           className="
             text-neutral-400
@@ -97,7 +136,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
           <Icon size={30} className="text-black" />
         </div>
         <AiFillStepForward
-          onClick={() => {}}
+          onClick={onPlayNext}
           size={30}
           className="
             text-neutral-400
@@ -106,6 +145,17 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
             transition
           "
         />
+      </div>
+
+      <div className="hidden md:flex w-full justify-end pr-2">
+        <div className="flex items-center gap-x-2 w-[120px]">
+          <VolumeIcon 
+            onClick={() => {}}
+            className="cursor-pointer"
+            size={34}
+          />
+          <Slider />         
+        </div>
       </div>
     </div>
   );
