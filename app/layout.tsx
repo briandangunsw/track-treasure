2f638f9f8d732002a58ee38e1189315e0725be05
briandangunsw <long.dang@student.unsw.edu.au>
2024-09-22 17:08:42 +1000

    updated SubscribeModal.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default async function RootLayout({
         <ToasterProvider />
         <SupabaseProvider>
           <UserProvider>
-            <ModalProvider />
+            <ModalProvider products={products}/>
             <Sidebar songs={userSongs}>
               {children}
             </Sidebar>
--- components/SubscribeModal.tsx
+++ components/SubscribeModal.tsx
@@ -1,13 +1,53 @@
 "use client";
 
-import Modal from "./Modal";
+import { Price, ProductWithPrice } from "@/types";
+import Modal from './Modal';
+import Button from './Button';
 
-const SubscribeModal = () => {
+interface SubscribeModalProps {
+  products: ProductWithPrice[];
+}
+
+const formatPrice = (price: Price) => {
+  const priceString = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: price.currency,
+    minimumFractionDigits: 0
+  }).format((price?.unit_amount || 0) / 100);
+
+  return priceString;
+}
+
+const SubscribeModal: React.FC<SubscribeModalProps> = ({
+  products
+}) => {
   let content = (
     <div className="text-center">
       No products available.
     </div>
   );
+
+  if (products?.length) {
+    content = (
+      <div>
+        {products.map((product) => {
+          if (!product.prices?.length) {
+            return (
+              <div key={product.id}>
+                No prices available
+              </div>
+            );
+          }
+
+          return product.prices.map((price) => (
+            <Button key={price.id}>
+              {`Subscribe for ${formatPrice(price)} a ${price.interval}`}
+            </Button>
+          ))
+        })}
+      </div>
+    )
+  }
 
   return (
     <Modal
